2a55047c4b4785268d8bc1e59998bbfcbe6af79c
Ahhiyawa1 <140977846+Ahhiyawa1@users.noreply.github.com>
2024-06-17 12:21:51 -0400

    First major push
    .

---
+++ common/defines/cpbm_defines.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NDiplomacy.TRUCE_YEARS = 5    		-- _DDEF_TRUCE_YEARS_; Years of Truce
+NDefines.NDiplomacy.SCALED_TRUCE_YEARS = 10		-- Additional years of truce based on % of warscore taken in war (100% warscore = full scaled truce years)
