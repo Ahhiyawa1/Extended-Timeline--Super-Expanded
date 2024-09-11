b37746603db9ead15ff6095a30d3d76d64837c4a
Ahhiyawa1 <140977846+Ahhiyawa1@users.noreply.github.com>
2024-09-10 22:34:05 -0400

    .....

--- common/defines/cpbm_defines.lua
+++ common/defines/cpbm_defines.lua
@@ -4,4 +4,4 @@
 
 NDefines.NDiplomacy.TRUCE_YEARS = 5    		-- _DDEF_TRUCE_YEARS_; Years of Truce
 NDefines.NDiplomacy.SCALED_TRUCE_YEARS = 10		-- Additional years of truce based on % of warscore taken in war (100% warscore = full scaled truce years)
-NDefines.NDiplomacy.MAX_WARSCORE = 250
+NDefines.NDiplomacy.MAX_WARSCORE = 300
