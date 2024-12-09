749b1fda419ec3e8d6bab1da21b41ee3edc2b662
Ahhiyawa1 <140977846+Ahhiyawa1@users.noreply.github.com>
2024-12-09 14:51:20 -0500

    ...

---
+++ common/defines/00_ImmortalAdvisors.lua
@@ -0,0 +1,5 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+NDefines.NCountry.MINIMUM_ADVISOR_DURATION = 30					-- _CDEF_MINIMUM_ADVISOR_DURATION_ Was 10.
+NDefines.NCountry.MINIMUM_ADVISOR_DURATION_CHANCE_VALUE = 150		-- Higher value here gives lower daily death chance after MINIMUM_ADVISOR_DURATION Was 15.
