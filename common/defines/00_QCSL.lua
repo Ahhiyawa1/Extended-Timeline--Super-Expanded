b1385a145b3259c8f5bb89d835aeebfac9d3a3af
Ahhiyawa1 <140977846+Ahhiyawa1@users.noreply.github.com>
2025-02-19 12:07:00 -0500

    ..

--- common/defines/00_QCSL.lua
+++ common/defines/00_QCSL.lua
@@ -4,9 +4,9 @@
 
 NDefines.NCountry.MONARCH_MIN_SKILL	= 4 -- Was 0, but I believe this is less annoying
 NDefines.NCountry.MONARCH_MAX_SKILL = 8 -- 24 from 6, quadrupled.
-NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0.0025 -- Decreased from 0.005
+NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0.00025 -- Decreased from 0.005
 NDefines.NCountry.MAXIMUM_ADVISOR_SKILL = 8 -- 10 from 5, quadrupled.
 NDefines.NCountry.MINIMUM_ADVISOR_DURATION = 18					-- _CDEF_MINIMUM_ADVISOR_DURATION_ Was 10.
 NDefines.NCountry.MINIMUM_ADVISOR_DURATION_CHANCE_VALUE = 30		-- Higher value here gives lower daily death chance after MINIMUM_ADVISOR_DURATION Was 15.
 NDefines.NCountry.PROMOTE_COST_MONTHS_ADVISOR_SALARY = 3.5 -- Was 10. This will encourage promotion.
-NDefines.NEconomy.ADVISOR_COST = 0.5 -- was 1. cheaper advisors
+NDefines.NEconomy.ADVISOR_COST = 0.3 -- was 1. cheaper advisors
